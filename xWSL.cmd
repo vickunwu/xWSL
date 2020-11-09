8f7088c39c1792c1379e1065b4d3f6fd81f19642
vickunwu <31735039+vickunwu@users.noreply.github.com>
2020-11-09 14:02:53 +0800

    Add files via upload

--- xWSL.cmd
+++ xWSL.cmd
@@ -1,4 +1,5 @@
 @ECHO OFF
+COLOR 1F
 SET GITORG=vickunwu
 SET GITPRJ=xWSL
 SET BRANCH=master
@@ -16,22 +17,22 @@ NET SESSION >NUL 2>&1
   )
 
 REM ## Enable WSL
-POWERSHELL.EXE -command "Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
-CLS && SET RUNSTART=%date% @ %time%
+POWERSHELL.EXE -command "dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart"
+POWERSHELL.EXE -command "dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart"
 
+REM ## Get installation parameters
+:DI
+CLS && SET RUNSTART=%date% @ %time%
+IF EXIST .\CMD.EXE CD ..\..
+ECHO [xWSL Installer]
+ECHO:
+ECHO Enter a unique name for the distro or hit Enter to use default 
+SET DISTRO=xWSL& SET /p DISTRO=Keep the name simple, no space or underscore characters [xWSL]: 
+IF EXIST %DISTRO%\*.VHDX GOTO DI
+IF EXIST %DISTRO%\rootfs GOTO DI
 REM ## Determine ideal DPI
 IF NOT EXIST "%DOWNLOADAREA%\dpi.ps1" POWERSHELL.EXE -ExecutionPolicy Bypass -Command "wget '%BASE%/dpi.ps1' -UseBasicParsing -OutFile '%DOWNLOADAREA%\dpi.ps1'"
 FOR /f "delims=" %%a in ('powershell -ExecutionPolicy bypass -command "%DOWNLOADAREA%\dpi.ps1" ') do set "LINDPI=%%a"
-
-REM ## Get installation parameters
-IF /I "%CD%"=="C:\Windows\System32" CD "%HOMEPATH%"
-ECHO xWSL Installer
-ECHO:
-
-:DI
-ECHO Enter a unique name for the distro or hit Enter to use default 
-SET DISTRO=xWSL& SET /p DISTRO=Keep the name simple, no space or underscore characters [xWSL]: 
-IF EXIST %DISTRO% GOTO DI
 ECHO:
                  SET /p LINDPI=Set custom DPI scale or hit Enter to use Windows value [%LINDPI%]: 
 SET RDPPRT=3399& SET /p RDPPRT=Port number for xRDP traffic or hit Enter to use default [3399]: 
@@ -45,52 +46,57 @@ SET GO="%DISTROFULL%\LxRunOffline.exe" r -n "%DISTRO%" -c
 ECHO:
 ECHO Download and install "%DISTRO%" to location "%DISTROFULL%" 
 IF NOT EXIST "%DOWNLOADAREA%\Ubuntu2004.zip" POWERSHELL.EXE -Command "Start-BitsTransfer -source https://aka.ms/wslubuntu2004 -destination '%DOWNLOADAREA%\Ubuntu2004.zip'"
-POWERSHELL.EXE -command "Expand-Archive -Path '%DOWNLOADAREA%\Ubuntu2004.zip' -DestinationPath '%DOWNLOADAREA%' -force
+POWERSHELL.EXE -command "Expand-Archive -Path '%DOWNLOADAREA%\Ubuntu2004.zip' -DestinationPath '%DOWNLOADAREA%'" -force
+%DISTROFULL:~0,1%: & MKDIR "%DISTROFULL%" & CD "%DISTROFULL%" & MKDIR logs & TakeOwn /f %DISTROFULL% /r /d y > NUL
 ECHO:
-ECHO Installing Distro Base...
+ECHO Installing Ubuntu 20.04...
 IF NOT EXIST "%DOWNLOADAREA%\LxRunOffline.exe" POWERSHELL.EXE -Command "wget %BASE%/LxRunOffline.exe -UseBasicParsing -OutFile '%DOWNLOADAREA%\LxRunOffline.exe'"
 START /WAIT /MIN "Installing Distro Base..." "%DOWNLOADAREA%\LxRunOffline.exe" "i" "-n" "%DISTRO%" "-f" "%DOWNLOADAREA%\install.tar.gz" "-d" "%DISTROFULL%"
-%DOWNLOADAREA%\LxRunOffline.exe sd -n "%DISTRO%"
-COPY "%DOWNLOADAREA%\LxRunOffline.*" "%DISTROFULL%" > NUL
+"%DOWNLOADAREA%\LxRunOffline.exe" sd -n "%DISTRO%"
+COPY "%DOWNLOADAREA%\LxRunOffline.exe" "%DISTROFULL%" > NUL
 ECHO:
 ECHO Add exclusions in Windows Defender if requested...
-IF NOT EXIST %DOWNLOADAREA%\excludeWSL.ps1 POWERSHELL.EXE -Command "wget %BASE%/excludeWSL.ps1 -UseBasicParsing -OutFile '%DOWNLOADAREA%\excludeWSL.ps1'"
-IF %DEFEXL%==X POWERSHELL.EXE -ExecutionPolicy bypass -command "%DOWNLOADAREA%\excludeWSL.ps1 '%DISTROFULL%'"
+POWERSHELL.EXE -Command "wget %BASE%/excludeWSL.ps1 -UseBasicParsing -OutFile '%DISTROFULL%\excludeWSL.ps1'"
+IF %DEFEXL%==X POWERSHELL.EXE -ExecutionPolicy bypass -Command ".\excludeWSL.ps1 '%DISTROFULL%'"
+DEL "%DISTROFULL%\excludeWSL.ps1"
 ECHO:
 ECHO Download xWSL overlay...
+(ECHO [xWSL Inputs] && ECHO. && ECHO.   Distro: %DISTRO% && ECHO.     Path: %DISTROFULL% && ECHO. RDP Port: %RDPPRT% && ECHO. SSH Port: %SSHPRT%  && ECHO.DPI Scale: %LINDPI% && ECHO.) > .\logs\Step0_Inputs.log
+
 CD "%DISTROFULL%"
 ECHO Change Repo
 %GO% "cp /etc/apt/sources.list /etc/apt/sources.list.bak"
 %GO% "sed -i 's/http:\/\/.*.ubuntu.com/https:\/\/mirrors.ustc.edu.cn/g' /etc/apt/sources.list"
 %GO% "apt-get update"
-ECHO xWSL INPUT SPECIFICATIONS // Path: %DISTROFULL% // Distro: %DISTRO% // RDP Port: %RDPPRT% // SSH Port: %SSHPRT% > Step0_Inputs.log
+
 %GO% "cd /tmp ; git clone -b %BRANCH% --depth=1 https://github.com/%GITORG%/%GITPRJ%.git"
-%GO% "ssh-keygen -A ; mkdir -p /root/.local/share ; apt-get update" > Step1_Update.log
+%GO% "ssh-keygen -A ; mkdir -p /root/.local/share ; apt-get update" > .\logs\Step1_Update.log
 ECHO:
-ECHO Install repo packages, please wait...
-%GO% "DEBIAN_FRONTEND=noninteractive apt-get -y install /tmp/xWSL/deb/gksu_2.1.0_amd64.deb /tmp/xWSL/deb/libgksu2-0_2.1.0_amd64.deb /tmp/xWSL/deb/libgnome-keyring0_3.12.0-1+b2_amd64.deb /tmp/xWSL/deb/libgnome-keyring-common_3.12.0-1_all.deb /tmp/xWSL/deb/multiarch-support_2.27-3ubuntu1_amd64.deb /tmp/xWSL/deb/xrdp_0.9.13.1-2_amd64.deb /tmp/xWSL/deb/xorgxrdp_0.2.12-1_amd64.deb /tmp/xWSL/deb/plata-theme_0.9.8-0ubuntu1~focal1_all.deb /tmp/xWSL/deb/papirus-icon-theme_20200901-4672+pkg21~ubuntu20.04.1_all.deb /tmp/xWSL/deb/fonts-cascadia-code_2005.15-1_all.deb --no-install-recommends" > Step2_InstallBase.log
+ECHO Install base packages, please wait...
+%GO% "DEBIAN_FRONTEND=noninteractive apt-get -y install /tmp/xWSL/deb/gksu_2.1.0_amd64.deb /tmp/xWSL/deb/libgksu2-0_2.1.0_amd64.deb /tmp/xWSL/deb/libgnome-keyring0_3.12.0-1+b2_amd64.deb /tmp/xWSL/deb/libgnome-keyring-common_3.12.0-1_all.deb /tmp/xWSL/deb/multiarch-support_2.27-3ubuntu1_amd64.deb /tmp/xWSL/deb/xrdp_0.9.13.1-2_amd64.deb /tmp/xWSL/deb/xorgxrdp_0.2.12-1_amd64.deb /tmp/xWSL/deb/plata-theme_0.9.8-0ubuntu1~focal1_all.deb /tmp/xWSL/deb/papirus-icon-theme_20200901-4672+pkg21~ubuntu20.04.1_all.deb /tmp/xWSL/deb/fonts-cascadia-code_2005.15-1_all.deb --no-install-recommends" > .\logs\Step2_BasePackages.log
 ECHO:
-ECHO Install dependencies for desktop environments...
-%GO% "DEBIAN_FRONTEND=noninteractive apt-get -y install x11-apps x11-session-utils x11-xserver-utils pulseaudio dialog distro-info-data lsb-release dumb-init inetutils-syslogd xdg-utils avahi-daemon libnss-mdns binutils putty synaptic pulseaudio-utils pulseaudio mesa-utils bzip2 p7zip-full unar unzip zip libatkmm-1.6-1v5 libcairomm-1.0-1v5 libcanberra-gtk3-0 libcanberra-gtk3-module libglibmm-2.4-1v5 libgtkmm-3.0-1v5 libpangomm-1.4-1v5 libsigc++-2.0-0v5 dbus-x11 libdbus-glib-1-2 libqt5core5a --no-install-recommends" > Step5_DesktopDeps.log
+ECHO Install dependencies for desktop environment...
+%GO% "DEBIAN_FRONTEND=noninteractive apt-get -y install x11-apps x11-session-utils x11-xserver-utils pulseaudio dialog distro-info-data lsb-release dumb-init inetutils-syslogd xdg-utils avahi-daemon libnss-mdns binutils putty synaptic pulseaudio-utils pulseaudio mesa-utils bzip2 p7zip-full unar unzip zip libatkmm-1.6-1v5 libcairomm-1.0-1v5 libcanberra-gtk3-0 libcanberra-gtk3-module libglibmm-2.4-1v5 libgtkmm-3.0-1v5 libpangomm-1.4-1v5 libsigc++-2.0-0v5 dbus-x11 libdbus-glib-1-2 libqt5core5a --no-install-recommends" > .\logs\Step5_DesktopDeps.log
 ECHO:
 ECHO Install XFCE4...
-%GO% "DEBIAN_FRONTEND=noninteractive apt-get -y install xfce4-terminal xfce4-whiskermenu-plugin xfce4-pulseaudio-plugin pavucontrol xfwm4 xfce4-panel xfce4-session xfce4-settings thunar thunar-volman thunar-archive-plugin xfdesktop4 xfce4-screenshooter libsmbclient gigolo gvfs-fuse gvfs-backends gvfs-bin mousepad evince xarchiver lhasa lrzip lzip lzop ncompress zip unzip dmz-cursor-theme adapta-gtk-theme gconf-defaults-service xfce4-taskmanager hardinfo --no-install-recommends" > Step6_XFCE4.log
+%GO% "DEBIAN_FRONTEND=noninteractive apt-get -y install xfce4-terminal xfce4-whiskermenu-plugin xfce4-pulseaudio-plugin pavucontrol xfwm4 xfce4-panel xfce4-session xfce4-settings thunar thunar-volman thunar-archive-plugin xfdesktop4 xfce4-screenshooter libsmbclient gigolo gvfs-fuse gvfs-backends gvfs-bin mousepad evince xarchiver lhasa lrzip lzip lzop ncompress zip unzip dmz-cursor-theme adapta-gtk-theme gconf-defaults-service xfce4-taskmanager hardinfo --no-install-recommends" > .\logs\Step6_XFCE4.log
 ECHO:
 ECHO Install Multimedia Components...
-%GO% "DEBIAN_FRONTEND=noninteractive apt-get -y install mtpaint parole" > Step7_Media.log
+%GO% "DEBIAN_FRONTEND=noninteractive apt-get -y install mtpaint parole" > .\logs\Step7_Media.log
 REM ## Additional items to install can go here...
 REM ## %GO% "cd /tmp ; wget https://files.multimc.org/downloads/multimc_1.4-1.deb"
 REM ## %GO% "apt-get -y install extremetuxracer tilix /tmp/multimc_1.4-1.deb"
-%GO% "apt-get -y install firefox tor"
+%GO% "apt-get -y install firefox-esr tor"
 %GO% "echo 'ExcludeNodes cn,hk,mo,kp,ir,sy,pk,cu,vn' | tee -a /etc/tor/torrc"
 %GO% "echo 'strictnodes 1' | tee -a /etc/tor/torrc"
 %GO% "apt-get -y install ibus-rime ; ibus restart ; ibus engine rime"
-%GO% "cd /tmp ; git clone https://github.com/jayknoxqu/ibus-rime.git ; cp -r ibus-rime/* ~/.config/ibus/rime ; ibus-daemon -drx"
+%GO% "cp -r /tmp/xWSL/dist/clover-pinyin/* ~/.config/ibus/rime ; touch ~/.config/ibus/rime/ ; ibus restart"
+%GO% "rm -rf /tmp/xWSL/dist/clover-pinyin"
 REM ## Things to do: Install Firefox; Install Rime; Install Tor;
 ECHO:
 ECHO Cleaning up...
 %GO% "rm -rf /etc/apt/apt.conf.d/20snapd.conf /etc/rc2.d/S01whoopsie /etc/init.d/console-setup.sh"
-%GO% "apt-get -qq purge cryptsetup cryptsetup-bin cryptsetup-initramfs cryptsetup-run irqbalance multipath-tools apparmor snapd squashfs-tools libplymouth5 plymouth plymouth-theme-ubuntu-text open-vm-tools cloud-init isc-dhcp-* gnustep* lvm2* mdadm apport open-iscsi powermgmt-base popularity-contest fwupd libfwupd2 ; apt-get -qq autoremove ; apt-get -qq clean" > Step8_Cleanup.log
+%GO% "apt-get -qq purge cryptsetup cryptsetup-bin cryptsetup-initramfs cryptsetup-run irqbalance multipath-tools apparmor snapd squashfs-tools libplymouth5 plymouth plymouth-theme-ubuntu-text open-vm-tools cloud-init isc-dhcp-* gnustep* lvm2* mdadm apport open-iscsi powermgmt-base popularity-contest fwupd libfwupd2 ; apt-get -qq autoremove ; apt-get -qq clean" > .\logs\Step8_Cleanup.log
 IF %LINDPI% GEQ 288 ( %GO% "sed -i 's/HISCALE/3/g' /tmp/xWSL/dist/etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml" )
 IF %LINDPI% GEQ 192 ( %GO% "sed -i 's/HISCALE/2/g' /tmp/xWSL/dist/etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml/xsettings.xml" )
 IF %LINDPI% GEQ 192 ( %GO% "sed -i 's/Default-hdpi/Default-xhdpi/g' /tmp/xWSL/dist/etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml" )
@@ -106,6 +112,7 @@ SET /A SESMAN = %RDPPRT% - 50
 %GO% "sed -i 's/#Port 22/Port %SSHPRT%/g' /etc/ssh/sshd_config"
 %GO% "sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config"
 %GO% "sed -i 's/XWSLINSTANCENAME/%DISTRO%/g' /tmp/xWSL/dist/usr/local/bin/initWSL"
+%GO% "sed -i 's/\\h/%DISTRO%/g' /tmp/xWSL/dist/etc/skel/.bashrc"
 %GO% "sed -i 's/#enable-dbus=yes/enable-dbus=no/g' /etc/avahi/avahi-daemon.conf ; sed -i 's/#host-name=foo/host-name=%COMPUTERNAME%-%DISTRO%/g' /etc/avahi/avahi-daemon.conf ; sed -i 's/use-ipv4=yes/use-ipv4=no/g' /etc/avahi/avahi-daemon.conf"
 %GO% "cp /mnt/c/Windows/Fonts/*.ttf /usr/share/fonts/truetype ; rm -rf /etc/pam.d/systemd-user ; rm -rf /etc/systemd ; rm -rf /usr/share/icons/breeze_cursors ; rm -rf /usr/share/icons/Breeze_Snow/cursors ; ssh-keygen -A ; adduser xrdp ssl-cert"
 %GO% "mv /usr/bin/pkexec /usr/bin/pkexec.orig ; echo gksudo -k -S -g \$1 > /usr/bin/pkexec ; chmod 755 /usr/bin/pkexec"
@@ -122,8 +129,8 @@ CD %DISTROFULL%
 ECHO:
 ECHO:
 SET /p XU=Enter name of %DISTRO% user: 
+POWERSHELL -Command $prd = read-host "Enter password for %XU%" -AsSecureString ; $BSTR=[System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($prd) ; [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR) > .tmp & set /p PWO=<.tmp
 BASH -c "useradd -m -p nulltemp -s /bin/bash %XU%"
-POWERSHELL -Command $prd = read-host "Enter password" -AsSecureString ; $BSTR=[System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($prd) ; [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR) > .tmp & set /p PWO=<.tmp
 BASH -c "echo %XU%:%PWO% | chpasswd"
 %GO% "sed -i 's/PLACEHOLDER/%XU%/g' /tmp/xWSL/xWSL.rdp"
 %GO% "sed -i 's/COMPY/%COMPUTERNAME%-%DISTRO%\.local/g' /tmp/xWSL/xWSL.rdp"
@@ -136,22 +143,36 @@ TYPE .tmp>.tmpsec.txt
 COPY /y /b xWSL._+.tmpsec.txt "%DISTROFULL%\%DISTRO% (%XU%) Desktop.rdp" > NUL
 DEL /Q  xWSL._ .tmp*.* > NUL
 BASH -c "echo '%XU% ALL=(ALL:ALL) ALL' >> /etc/sudoers"
-
-REM ## Open Firewall Ports
+ECHO:
+ECHO Open Windows Firewall Ports for xRDP, SSH, mDNS...
 NETSH AdvFirewall Firewall add rule name="%DISTRO% xRDP" dir=in action=allow protocol=TCP localport=%RDPPRT% > NUL
 NETSH AdvFirewall Firewall add rule name="%DISTRO% Secure Shell" dir=in action=allow protocol=TCP localport=%SSHPRT% > NUL
 NETSH AdvFirewall Firewall add rule name="%DISTRO% Avahi Multicast DNS" dir=in action=allow program="%DISTROFULL%\rootfs\usr\sbin\avahi-daemon" enable=yes > NUL
-
-REM ## Build RDP, Console, Init Links, Scheduled Task...
-ECHO @WSLCONFIG /t %DISTRO% > "%DISTROFULL%\Init.cmd"
-ECHO @WSL ~ -u root -d %DISTRO% -e initWSL 2 >> "%DISTROFULL%\Init.cmd"
-ECHO @WSL ~ -u %XU% -d %DISTRO%               > "%DISTROFULL%\%DISTRO% (%XU%) Console.cmd"
-COPY /Y "%DISTROFULL%\%DISTRO% (%XU%) Console.cmd" "%USERPROFILE%\Desktop\WSL Console.cmd" > NUL
-COPY /Y "%DISTROFULL%\%DISTRO% (%XU%) Desktop.rdp" "%USERPROFILE%\Desktop\WSL Desktop.rdp" > NUL
 START /MIN "%DISTRO% Init" WSL ~ -u root -d %DISTRO% -e initWSL 2
+ECHO Building RDP Connection file, Console link, Init system...
+ECHO @WSLCONFIG /t %DISTRO% >  "%DISTROFULL%\Init.cmd"
+ECHO @WSL ~ -u root -d %DISTRO% -e initWSL 2 >> "%DISTROFULL%\Init.cmd"
+ECHO @WSL ~ -u %XU% -d %DISTRO% > "%DISTROFULL%\%DISTRO% (%XU%) Console.cmd"
+POWERSHELL -Command "Copy-Item '%DISTROFULL%\%DISTRO% (%XU%) Console.cmd' ([Environment]::GetFolderPath('Desktop'))"
+POWERSHELL -Command "Copy-Item '%DISTROFULL%\%DISTRO% (%XU%) Desktop.rdp' ([Environment]::GetFolderPath('Desktop'))"
+ECHO Building Uninstaller... [%DISTROFULL%\%DISTRO%_Uninstall.cmd]
+ECHO @COLOR 1F                                                                                                   >  "%DISTROFULL%\Uninstall %DISTRO%.cmd"
+ECHO @ECHO Uninstall %DISTRO%?                                                                                   >> "%DISTROFULL%\Uninstall %DISTRO%.cmd"
+ECHO @PAUSE                                                                                                      >> "%DISTROFULL%\Uninstall %DISTRO%.cmd"
+ECHO @COPY /Y "%DISTROFULL%\LxRunOffline.exe" "%DOWNLOADAREA%"                                                           >> "%DISTROFULL%\Uninstall %DISTRO%.cmd"
+ECHO @POWERSHELL -Command "Remove-Item ([Environment]::GetFolderPath('Desktop')+'\%DISTRO% (%XU%) Console.cmd')" >> "%DISTROFULL%\Uninstall %DISTRO%.cmd"
+ECHO @POWERSHELL -Command "Remove-Item ([Environment]::GetFolderPath('Desktop')+'\%DISTRO% (%XU%) Desktop.rdp')" >> "%DISTROFULL%\Uninstall %DISTRO%.cmd"
+ECHO @SCHTASKS /Delete /TN:%DISTRO% /F                                                                           >> "%DISTROFULL%\Uninstall %DISTRO%.cmd"
+ECHO @CLS                                                                                                        >> "%DISTROFULL%\Uninstall %DISTRO%.cmd"
+ECHO @CD ..                                                                                                      >> "%DISTROFULL%\Uninstall %DISTRO%.cmd"
+ECHO @ECHO Uninstalling %DISTRO%, please wait...                                                                 >> "%DISTROFULL%\Uninstall %DISTRO%.cmd"
+ECHO @WSLCONFIG /T %DISTRO%                                                                                      >> "%DISTROFULL%\Uninstall %DISTRO%.cmd"
+ECHO @"%DOWNLOADAREA%\LxRunOffline.exe" ur -n %DISTRO%                                                                   >> "%DISTROFULL%\Uninstall %DISTRO%.cmd"
+ECHO @RD /S /Q "%DISTROFULL%"                                                                                    >> "%DISTROFULL%\Uninstall %DISTRO%.cmd"
+ECHO Building Scheduled Task...
 POWERSHELL -C "$WAI = (whoami) ; (Get-Content .\rootfs\tmp\xWSL\xWSL.xml).replace('AAAA', $WAI) | Set-Content .\rootfs\tmp\xWSL\xWSL.xml"
 POWERSHELL -C "$WAC = (pwd)    ; (Get-Content .\rootfs\tmp\xWSL\xWSL.xml).replace('QQQQ', $WAC) | Set-Content .\rootfs\tmp\xWSL\xWSL.xml"
-SCHTASKS /Create /TN:%distro% /XML .\rootfs\tmp\xWSL\xWSL.xml /F
+SCHTASKS /Create /TN:%DISTRO% /XML .\rootfs\tmp\xWSL\xWSL.xml /F
 ECHO:
 ECHO:      Start: %RUNSTART%
 ECHO:        End: %RUNEND%
@@ -169,5 +190,6 @@ PING -n 6 LOCALHOST > NUL
 REM ## Convert to WSL2
 CMD "wsl --set-version %DISTRO% 2"
 START "Remote Desktop Connection" "MSTSC.EXE" "/V" "%DISTROFULL%\%DISTRO% (%XU%) Desktop.rdp"
+CD ..
 ECHO: 
 :ENDSCRIPT
